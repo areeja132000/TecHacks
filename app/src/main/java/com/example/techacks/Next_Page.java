c7f0b73541bb433939cafafe9960c00172d78724
areeja132000 <areej.ali@mail.utoronto.ca>
2020-08-14 22:07:00 -0400

    Example of how to link pages
    Also contains xml file examples, java class examples, the whole thing.

--- app/src/main/java/com/example/techacks/Next_Page.java
+++ app/src/main/java/com/example/techacks/Next_Page.java
@@ -1,23 +1,22 @@
 package com.example.techacks;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-
+public class Next_Page extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.next_page);
 
         Button nextButton = (Button) findViewById(R.id.next_page);
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Next_Page.class);
+                Intent intent = new Intent(Next_Page.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }

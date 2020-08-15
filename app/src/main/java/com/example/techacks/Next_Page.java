7576a1635815d354f1590ca0e434fcf77d6ba797
areeja132000 <areej.ali@mail.utoronto.ca>
2020-08-15 17:48:14 -0400

    Added text recognition and photo taking capabilities
    Don't have time

--- app/src/main/java/com/example/techacks/Next_Page.java
+++ app/src/main/java/com/example/techacks/Next_Page.java
@@ -1,10 +1,26 @@
 package com.example.techacks;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
 
 public class Next_Page extends AppCompatActivity {
     @Override
@@ -16,10 +32,73 @@ public class Next_Page extends AppCompatActivity {
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Next_Page.this, MainActivity.class);
-                startActivity(intent);
-                finish();
+                dispatchTakePictureIntent();
             }
         });
     }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView image = (ImageView) findViewById(R.id.imageView);
+            image.setImageBitmap(imageBitmap);
+
+            InputImage imageToPass = InputImage.fromBitmap(imageBitmap, 0);
+
+            TextRecognizer recognizer = TextRecognition.getClient();
+
+            Task<Text> result =
+                    recognizer.process(imageToPass)
+                            .addOnSuccessListener(new OnSuccessListener<Text>() {
+                                        @Override
+                                        public void onSuccess(Text visionText) {
+                                            String endResult = processTextBlock(visionText);
+                                            TextView myAwesomeTextView = (TextView)findViewById(R.id.recognizedText);
+                                            myAwesomeTextView.setText(endResult);
+                                        }
+                                    })
+                            .addOnFailureListener(
+                                    new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            // Task failed with an exception
+                                            // ...
+                                        }
+                                    });
+
+        }
+    }
+
+    private String processTextBlock(Text result) {
+        //String resultText = result.getText();
+        return result.getText();
+//        for (Text.TextBlock block : result.getTextBlocks()) {
+//            String blockText = block.getText();
+//            Point[] blockCornerPoints = block.getCornerPoints();
+//            Rect blockFrame = block.getBoundingBox();
+//            for (Text.Line line : block.getLines()) {
+//                String lineText = line.getText();
+//                Point[] lineCornerPoints = line.getCornerPoints();
+//                Rect lineFrame = line.getBoundingBox();
+//                for (Text.Element element : line.getElements()) {
+//                    String elementText = element.getText();
+//                    Point[] elementCornerPoints = element.getCornerPoints();
+//                    Rect elementFrame = element.getBoundingBox();
+//                }
+//            }
+//        }
+    }
+
+
+
 }

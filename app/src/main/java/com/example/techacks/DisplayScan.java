a0ada6b50eebdfd71605aab9785344770998ebad
areeja132000 <areej.ali@mail.utoronto.ca>
2020-08-16 00:26:47 -0400

    Comments. Not important

--- app/src/main/java/com/example/techacks/DisplayScan.java
+++ app/src/main/java/com/example/techacks/DisplayScan.java
@@ -15,6 +15,16 @@ public class DisplayScan extends AppCompatActivity {
         setContentView(R.layout.display_scan);
         Bundle bundle = getIntent().getExtras();
         String scanned = bundle.getString("scan");
+
+//        String[] parts = scanned.split("\n");
+//
+//        String newString = "";
+//        for (int i = 0; i < parts.length; i++) {
+//            // accessing each element of array
+//            String x = parts[i];
+//            newString = newString + x + "\n";
+//        }
+
         TextView scanned_output = (TextView)findViewById(R.id.textView);
         scanned_output.setText(scanned);
     }
---
+++ app/src/main/java/com/example/techacks/dataBase.java
@@ -0,0 +1,4 @@
+package com.example.techacks;
+
+public class dataBase {
+}
